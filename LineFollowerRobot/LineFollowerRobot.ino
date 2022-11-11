d23cd2292f4f2aec570b8e17ccff1c593702e372
Anrry Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2022-11-11 12:23:08 -0300

    Update LineFollowerRobot.ino

--- LineFollowerRobot/LineFollowerRobot.ino
+++ LineFollowerRobot/LineFollowerRobot.ino
@@ -1,9 +1,53 @@
-void setup() {
-  // put your setup code here, to run once:
+// Sensores
+#define leftS A1
+#define rightS A2
+#define centerS A3
 
+// Motores
+#define leftM 4
+#define rightM 3
+
+// Setas
+#define leftA 10
+#define rightA 9
+
+// Freio
+#define bothB 11
+
+void setup() {
+  pinMode(leftS, INPUT);
+  pinMode(rightS, INPUT);
+  pinMode(leftM, OUTPUT);
+  pinMode(rightM, OUTPUT);
+  pinMode(leftA, OUTPUT);
+  pinMode(rightA, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (analogRead(leftS) > 90) {
+    digitalWrite(leftM, HIGH);
+    digitalWrite(leftA, LOW);
+  } else {
+    digitalWrite(leftM, LOW);
+    digitalWrite(leftA, HIGH);
+  }
 
+
+  if (analogRead(rightS) > 90) {
+    digitalWrite(rightM, HIGH);
+    digitalWrite(rightA, LOW);
+  } else {
+    digitalWrite(rightM, LOW);
+    digitalWrite(rightA, HIGH);
+  }
+
+  if (analogRead(rightS) > 90 && analogRead(centerS) > 90 && analogRead(leftS) > 90) {
+    digitalWrite(rightM, LOW);
+    digitalWrite(leftM, LOW);
+    digitalWrite(bothB, HIGH);
+  } else {
+    digitalWrite(bothB, LOW);
+  }
 }
+
+// abracadabra, é aqui que a magia acontece
